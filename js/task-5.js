274179eb87aaf907d3e425af759bfa8ce39fb7ab
natalia mohyla <mogilesha81@gmail.com>
2025-01-17 22:09:40 +0100

    task4

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const colorSpan = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+
+changeColorButton.addEventListener('click', () => {
+  const randomColor = getRandomHexColor(); 
+  body.style.backgroundColor = randomColor; 
+  colorSpan.textContent = randomColor; 
+});
+body.style.display = 'block';
+body.style.flexDirection = 'column';
+body.style.padding = '100px 88px';
+body.style.width = '345px';
+body.style.height = '280px';
+body.style.alignItems = 'center';
+
+changeColorButton.style.backgroundColor
+changeColorButton.style.width = '148px';
+changeColorButton.style.height = '40px';
+changeColorButton.style.borderRadius = '8px';
+changeColorButton.addEventListener('mouseover', () => {
+  changeColorButton.style.backgroundColor = '#6c8cff'; 
+});
+
+changeColorButton.addEventListener('mouseout', () => {
+  changeColorButton.style.backgroundColor = '#4e75ff'; 
+});
